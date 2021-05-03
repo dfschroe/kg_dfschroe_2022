cfab99ef41e7a61d5eeabd3a1d2bb183e87ed28d
David Schroeder <60638957+dfschroe@users.noreply.github.com>
2021-05-02 21:43:06 -0700

    comments
    Added line by line comments

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var input=process.argv.slice(2);
-var transformedArray=[];
-var dict = {};
-var output="";
+var input=process.argv.slice(2);//initiating input to store command line arguments
+var transformedArray=[];//initiating array
+var dict = {};//initiating dictionary
+var output=""; //initiating output str
 
-dict["0"]="Zero";
+dict["0"]="Zero"; //creating dictionary
 dict["1"]="One";
 dict["2"]="Two";
 dict["3"]="Three";
@@ -13,24 +13,25 @@ dict["6"]="Six";
 dict["7"]="Seven";
 dict["8"]="Eight";
 dict["9"]="Nine";
+
 for(var i=0; i<input.length;i++){
-  transformedArray.push(toStr(input[i],dict));
+  transformedArray.push(toStr(input[i],dict));//pushing each int to toStr to convert to phonetic
 }
-for(var i=0;i<transformedArray.length;i++){
+for(var i=0;i<transformedArray.length;i++){//print loop
   if(!(i==transformedArray.length-1)){
-    output+=transformedArray[i]+",";
+    output+=transformedArray[i]+",";//adding each transformedArray element to the output str
   }
   else{
-    output+=transformedArray[i];
+    output+=transformedArray[i]; //adding last element of transformedArray to ouput str
   }
 }
 console.log(output)
 
 function toStr(input,dict){
-  var output="";
+  var output="";//initializing output of function
   for(var i=0;i<input.length;i++){
-    output+=dict[input[i]];
+    output+=dict[input[i]];//adding each digit from dict into output str
   }
 
-  return output;
+  return output;//returning
 }
